dece107ced2aa13d1cd3ff991a1d8e926ed4b1c5
mrfabkirox <fabrice.esope@gmail.com>
2020-07-12 18:27:38 +0800

    paper fixed

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Paper from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 export default function Job({job}) {
   return (
-    <div className={'job'}>
+    <Paper className={'job'}>
       <div>
         <Typography variant='h6'>{job.title}</Typography>
         <Typography variant='h5'>{job.company}</Typography>
@@ -13,7 +13,7 @@ export default function Job({job}) {
       <div>
         <Typography variant='h5'>{job.created_at.split(' ').slice(0,3).join(' ')}</Typography>
       </div>
-    </div>
+    </Paper>
 
   )
 }
---
+++ src/JobModal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
+
+export default function JobModal({job}) {
+  return (
+    
+  )
+}
