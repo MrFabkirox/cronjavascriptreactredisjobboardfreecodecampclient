257e0a8504a4e07911d3dd28880ffbfd7cff00aa
mrfabkirox <fabrice.esope@gmail.com>
2020-07-12 13:40:40 +0800

    Jobs.js not here ? 'activeStep' to fix..3, client

--- src/App.js
+++ src/App.js
@@ -2,22 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Jobs from './Jobs';
+
+const JOB_API_URL = 'http://localhost:3001/jobs';
+
+const mockJobs = [
+  {title: 'SWE 1', company: 'Google'},
+  {title: 'SWE 1', company: 'facebook'},
+  {title: 'SWE 1', company: 'Apple'}
+]
+
+async function fetchJobs(updateCb) {
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+
+  console.log({json});
+
+  updateCb(json);
+}
+
 function App() {
+
+  const [jobList, updateJobs] = React.useState([])
+
+  React.useEffect(() => {
+    fetchJobs(updateJobs);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <Jobs jobs={jobList} />
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
---
+++ src/Job.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Paper from '@material-ui/core/Typography';
+import Typography from '@material-ui/core/Typography';
+
+export default function Job({job}) {
+  return (
+    <div className={'job'}>
+      <div>
+        <Typography variant='h6'>{job.title}</Typography>
+        <Typography variant='h5'>{job.company}</Typography>
+        <Typography variant='h5'>{job.location}</Typography>
+      </div>
+      <div>
+        <Typography variant='h5'>{job.created_at.split(' ').slice(0,3).join(' ')}</Typography>
+      </div>
+    </div>
+
+  )
+}
